4b7b45c9542231c62a3cbda62352792ef0ce5ab6
Daniel E. Renfer <duck@kronkltd.net>
2012-07-24 22:23:01 -0400

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ciste-incubator
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject ciste/ciste-incubator "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://github.com/duck1123/ciste-incubator"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [ciste "0.4.0-SNAPSHOT"]])
