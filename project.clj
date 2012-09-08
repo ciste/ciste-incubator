9f6085b557b627dc955f586229bc1493f2e47666
Daniel E. Renfer <duck@kronkltd.net>
2012-09-07 22:17:28 -0400

    bump dependencies

--- project.clj
+++ project.clj
@@ -1,11 +1,9 @@
 (defproject ciste/ciste-incubator "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Misc. Ciste code"
   :url "http://github.com/duck1123/ciste-incubator"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [ciste/ciste-core "0.4.0-SNAPSHOT"]
                  [org.clojure/tools.nrepl "0.2.0-beta8"]
-                 [org.clojure/tools.logging "0.2.3"]
-
-                 ])
+                 [org.clojure/tools.logging "0.2.4"]])
