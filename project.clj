1422936a43310360921838dc6326e6de66f481d1
Daniel E. Renfer <duck@kronkltd.net>
2012-09-08 00:00:58 -0400

    dev dependencies and plugins

--- project.clj
+++ project.clj
@@ -6,4 +6,10 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [ciste/ciste-core "0.4.0-SNAPSHOT"]
                  [org.clojure/tools.nrepl "0.2.0-beta8"]
-                 [org.clojure/tools.logging "0.2.4"]])
+                 [org.clojure/tools.logging "0.2.4"]]
+  :profiles {:dev
+             {:dependencies
+              [[log4j "1.2.17"]
+               [midje "1.4.0"]]}}
+  :plugins [[lein-midje "2.0.0-SNAPSHOT"]
+            [codox "0.6.1"]])
