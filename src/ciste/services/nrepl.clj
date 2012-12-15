d493928c65fa91800f375ef013beaa94c81e2367
Daniel E. Renfer <duck@kronkltd.net>
2012-12-15 17:47:13 -0500

    log when nrepl server has started

--- src/ciste/services/nrepl.clj
+++ src/ciste/services/nrepl.clj
@@ -1,12 +1,13 @@
 (ns ciste.services.nrepl
   (:use [ciste.config :only [config describe-config]]
-        [clojure.tools.nrepl.server :only (start-server stop-server)]
-        )
-  )
+        [clojure.tools.logging :as log]
+        [clojure.tools.nrepl.server :only [start-server stop-server]]))
 
 (defn start
   []
-  (start-server :port (config ::port)))
+  (let [port (config ::port)]
+    (log/infof "Starting nrepl server on port %s" port)
+    (start-server :port port)))
 
 (defn stop
   [])
