5325972ad1172a24d52508c06c05ee3188e0389d
Daniel E. Renfer <duck@kronkltd.net>
2012-12-22 12:20:18 -0500

    bump dependencies

--- project.clj
+++ project.clj
@@ -5,11 +5,11 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [ciste/ciste-core "0.4.0-SNAPSHOT"]
-                 [org.clojure/tools.nrepl "0.2.0-beta8"]
+                 [org.clojure/tools.nrepl "0.2.0-beta9"]
                  [org.clojure/tools.logging "0.2.4"]]
   :profiles {:dev
              {:dependencies
               [[log4j "1.2.17"]
-               [midje "1.4.0"]]}}
-  :plugins [[lein-midje "2.0.0-SNAPSHOT"]
-            [codox "0.6.1"]])
+               [midje "1.5-alpha3"]]}}
+  :plugins [[lein-midje "2.0.3"]
+            [codox "0.6.4"]])
