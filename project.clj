526afb7f054d688444cde818673c96af20d24af4
Daniel E. Renfer <duck@kronkltd.net>
2012-12-22 12:26:40 -0500

    use ciste

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [ciste/ciste-core "0.4.0-SNAPSHOT"]
+                 [ciste "0.4.0-SNAPSHOT"]
                  [org.clojure/tools.nrepl "0.2.0-beta9"]
                  [org.clojure/tools.logging "0.2.4"]]
   :profiles {:dev
