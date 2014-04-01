d3bd916455357a25f6075bcd16dfbbdc75f392dc
Daniel E. Renfer <duck@kronkltd.net>
2014-03-31 20:46:43 -0400

    update dependencies

--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
   :url "http://github.com/duck1123/ciste-incubator"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [ciste "0.4.0-SNAPSHOT"]
-                 [org.clojure/tools.nrepl "0.2.0-beta9"]
-                 [org.clojure/tools.logging "0.2.4"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [ciste "0.5.0-SNAPSHOT"]
+                 [org.clojure/tools.nrepl "0.2.3"]
+                 [org.clojure/tools.logging "0.2.6"]]
   :profiles {:dev
              {:dependencies
               [[log4j "1.2.17"]
-               [midje "1.5-alpha3"]]}}
-  :plugins [[lein-midje "2.0.3"]
-            [codox "0.6.4"]])
+               [midje "1.6.3"]]}}
+  :plugins [[lein-midje "3.1.3"]
+            [codox "0.6.7"]])
