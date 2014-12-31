702732e528fe3df122853adbab24369e09b265de
Daniel E. Renfer <duck@kronkltd.net>
2014-12-30 22:59:56 -0500

    update deps

--- project.clj
+++ project.clj
@@ -5,8 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [ciste "0.5.0-SNAPSHOT"]
-                 [org.clojure/tools.nrepl "0.2.3"]
-                 [org.clojure/tools.logging "0.2.6"]]
+                 [org.clojure/tools.nrepl "0.2.6"]
+                 [org.clojure/tools.logging "0.3.1"]]
   :profiles {:dev
              {:dependencies
               [[log4j "1.2.17"]
