7163e50957a31a0c4033420c19b6b6f2ac5c3898
Daniel E. Renfer <duck@kronkltd.net>
2016-03-12 16:01:05 -0500

    update dependencies

--- project.clj
+++ project.clj
@@ -4,15 +4,15 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [cider/cider-nrepl "0.10.0-SNAPSHOT"]
+                 [cider/cider-nrepl "0.11.0"]
                  [ciste "0.6.0-SNAPSHOT"]
                  [http-kit "2.1.19"]
-                 [org.clojure/tools.nrepl "0.2.11"]
+                 [org.clojure/tools.nrepl "0.2.12"]
                  [org.clojure/tools.logging "0.3.1"]]
   :profiles {:dev
              {:dependencies
               [[log4j "1.2.17"]
-               [midje "1.7.0"]]
+               [midje "1.8.3"]]
               :plugins [[lein-ancient "0.6.7"]]}}
   :plugins [[lein-midje "3.1.3"]
             [codox "0.8.10"]])
